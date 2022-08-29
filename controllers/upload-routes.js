b92d70f3cc353bd274c99f15048ebc53b1c8132e
May Carandang <maytiara@gmail.com>
2022-08-29 23:01:57 +0800

    let see if it works

--- controllers/upload-routes.js
+++ controllers/upload-routes.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const path = require('path');
-
-// This is the 'get' route 
-router.get('/', async (req, res) => {
-    // Add a comment describing the purpose of the render method
-    // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
-    res.render('all');
-}); 
-
-module.exports = router;
+// const router = require('express').Router();
+// const path = require('path');
+// 
+// // This is the 'get' route 
+// router.get('/', async (req, res) => {
+//     // Add a comment describing the purpose of the render method
+//     // This method is rendering the 'all' Handlebars.js template. This is how we connect each route to the correct template.
+//     res.render('all');
+// }); 
+// 
+// module.exports = router;
