cc7e08bebbc3feb7c2a60721b68fa38570b27850
May Carandang <maytiara@gmail.com>
2022-08-29 22:09:34 +0800

    few stuffs required for this app

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS judge_my_stuff_db;
 CREATE DATABASE judge_my_stuff_db;
 
-CREATE TABLE `judge_my_stuff_db`.`user` (
+/* CREATE TABLE `judge_my_stuff_db`.`user` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `profile_image` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`)); */
