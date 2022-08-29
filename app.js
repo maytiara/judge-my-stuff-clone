7c18b580dbbf9d3f869b7811bb524021c4a602a4
May Carandang <maytiara@gmail.com>
2022-08-29 22:09:55 +0800

    main entrypoint of the app

--- app.js
+++ app.js
@@ -1,67 +1,58 @@
 const path = require('path');
 const express = require('express'); // npm 
-//const routes = require('./routes');
-// const mysql = require('mysql2');
-const exhbs= require('express-handlebars');
-
-const fileUpload = require('express-fileupload'); // npm to upload files
+//const db = require('mysql2');
+const exhbs= require('express-handlebars'); 
+// const routes = require('./controllers/upload-routes')
 const sequelize = require('./config/connection');
+const cloudinary = require('cloudinary').v2; // declare it later..
+
+const session = require('express-session'); // required for session.Store
+// const router = require('./routes/indexRoutes');
+const SequelizeStore = require('connect-session-sequelize')(session.Store); // required for const = sess
+const bodyParser = require('body-parser');
+
+// Added npm, this requires to use the cookie-sessions.
+// Source: Wk14,mini-proj (line 9, 17-27)
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  
+  store: new SequelizeStore({
+    db: sequelize
+  }) 
+ 
+};
 
 const app = express(); //
+app.use(session(sess)); // to initiate the const sess
 const PORT = process.env.PORT || 3001; //localhost port
-
-// default options  ---express-fileupload----
-app.use(fileUpload());
 
 // Express middleware
 // Static files: required for express -> to call the files inside the public folder
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended:true })); // to Parse URL data from any form
 app.use(express.static(path.join(__dirname,'public')));
 
 // this connects to the ./routes folder --to route the request
-//app.use(routes);
+// Need to define the routes
+app.use(require('./routes/cloudinary-routes'));
+app.use('/userRoutes', require('./routes/web/userRoutes')); // middleware for login.hbs
 
-// template for handlebars ./view
+app.use('/', require('./routes/indexRoutes')); // middleware for the rest of the routes
+//app.use('/', require('./routes/signupRoutes')); // middleware for the rest of the routes
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Handlebars middleware
 // Inform Express.js on which template engine to use
-app.engine('hbs', exhbs.engine({ extname: '.hbs'})); // inside the { is the option property }
+app.engine('hbs', exhbs.engine({ extname: '.hbs'})); // inside the { config }
 app.set('view engine', 'hbs');
-
-// this is the router
-app.get('', (req, res) => {
-  res.render('index');
-
-
-});
-
-
-// next route, POST ---express-fileupload----
-app.post('', (req, res) => {
-  let sampleFile;
-  let uploadPath;
-
-  if(!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded'); // this will return when the user didn't upload any files
-  }
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/upload/' + sampleFile.name; // uploadPath directory
-
-
-  console.log(sampleFile); // you can omit the comment to see on your console log the mv property key value:
-
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
-
-    res.send('File Uploaded'); // it will send the message, once the user upload the folder
-  });
-});
+app.use(express.static('./public/images'));
 
 // turn on connection server
-sequelize.sync({ force: false }).then
+sequelize.sync({ force: false }).then 
 (() => {
   app.listen(PORT, () =>
     console.log('Now listening 🚀'));
